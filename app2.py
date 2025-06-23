66eb9a5fef8d675a0edcdb8056a82f6a56c10df8
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-06-23 16:29:19 -0300

    Atualizar o app2.py

--- app2.py
+++ app2.py
@@ -1,15 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = "Chave-secreta"  
+app.secret_key = 'Chave-secreta'
+
+# Usuário e senha fixos
+USUARIO_CORRETO = 'Camilo'
+SENHA_CORRETA = 'Huxiaobestship'
+
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
-    session['username'] = username 
-    return redirect(url_for('login'))
+    password = request.form['password']
 
-@app.route('/profile')
+    if username == USUARIO_CORRETO and password == SENHA_CORRETA:
+        session['username'] = username
+        return redirect(url_for('produto.html'))
+    else:
+        return '<h2>Usuário ou senha incorreta, tente novamente.</h2>'
+
+@app.route('/produto')
 def profile():
-    username = session.get('username')
-    return render_template('login.html', username=username)
+    if 'username' in session:
+        username = session['username']
+        return f'<h2>Bem-vindo ao perfil, {username}!</h2>'
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
